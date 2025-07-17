452f43f205a466ebfd21c02d35df9e550ba71815
MF-Ahmed <itsfarhan@outlook.com>
2025-07-17 18:43:39 +0200

    added new files

--- drone_ship/aquabot_gz/build/aquabot_gz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/data/ros2_ws/src/drone_ship/aquabot_gz
-# Build directory: /home/user/data/ros2_ws/src/drone_ship/aquabot_gz/build/aquabot_gz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
